8b7a301f48b117b405ef8cedee8511e16667236a
Marwanbz <kaka.mer1998@gmail.com>
2025-05-18 23:56:43 +0300

    feat: add react router & create about route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import './index.css'
 
+import { BrowserRouter, Route, Routes } from "react-router";
 import {
   QueryClient,
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query'
 
+import About from './components/About.tsx';
 import App from './App.tsx'
+import ReactDOM from "react-dom/client";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
@@ -15,7 +18,14 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 )
